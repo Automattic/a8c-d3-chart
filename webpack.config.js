aab4f409b44149b56d2c3c2dec9304e9d8c91ddf
Robert Elliott <robert.elliott@automattic.com>
2018-11-22 16:26:26 +0200

    Remove the now ignored directory node_modules

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|bower_components|build)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env','@babel/react']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    externals: {
+        'react': 'commonjs react'
+    }
+};
